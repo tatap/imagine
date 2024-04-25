fb4af3176d216f51cca0efe7da015e54b39be805
tatap <punwiphat@gmail.com>
2024-04-25 17:29:56 +0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
+
+export const metadata: Metadata = {
+  title: "Imaginify app",
+  description: "AI-powered image generato",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={
+      {
+        variables: { colorPrimary: '#624cf5'}
+      }
+    }>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
